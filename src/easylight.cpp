bd1fe8f9430460e875141a46b0c8891c3b88edbb
Qianmo <92790790+Qianmoxsn@users.noreply.github.com>
2023-10-21 14:45:21 +0800

    Add: test ware and easylight.h

---
+++ src/easylight.cpp
@@ -0,0 +1,142 @@
+#include "easylight.h"
+
+/**
+ * @brief Initializes the pins for the LEDs.
+ *
+ */
+void setupLights() {
+  for (int i = 0; i < 5; i++) {
+    pinMode(led_pin_list[i], OUTPUT);
+  }
+}
+
+/**
+ * @brief Flows the LEDs with a delay of 100ms.
+ *
+ */
+void flowlights() { flowlights(100); }
+
+/**
+ * @brief Flows the LEDs with a given delay.
+ *
+ * @param m_sec The delay in milliseconds.
+ */
+
+void flowlights(int m_sec) {
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(led_pin_list[i], LED_ON);
+    delay(m_sec);
+    digitalWrite(led_pin_list[i], LED_OFF);
+    delay(m_sec);
+  }
+}
+
+/**
+ * Turns on all 5 LEDs.
+ */
+void turnOn() {
+  for (int i = 0; i < 5; i++) {
+    turnOn(i + 1);
+  }
+}
+
+/**
+ * Turns off all 5 LEDs.
+ */
+void turnOff() {
+  for (int i = 0; i < 5; i++) {
+    turnOff(i + 1);
+  }
+}
+
+/**
+ * Turns on a specific LED.
+ *
+ * @param ledx The index of the LED to turn on (1-5).
+ */
+void turnOn(int ledx) { digitalWrite(led_pin_list[ledx - 1], LED_ON); }
+
+/**
+ * Turns off a specific LED.
+ *
+ * @param ledx The index of the LED to turn off (1-5).
+ */
+void turnOff(int ledx) { digitalWrite(led_pin_list[ledx - 1], LED_OFF); }
+
+/**
+ * Turns on two specific LEDs.
+ *
+ * @param ledx1 The index of the first LED to turn on (1-5).
+ * @param ledx2 The index of the second LED to turn on (1-5).
+ */
+void turnOn(int ledx1, int ledx2) {
+  turnOn(ledx1);
+  turnOn(ledx2);
+}
+
+/**
+ * Turns off two specific LEDs.
+ *
+ * @param ledx1 The index of the first LED to turn off (1-5).
+ * @param ledx2 The index of the second LED to turn off (1-5).
+ */
+void turnOff(int ledx1, int ledx2) {
+  turnOff(ledx1);
+  turnOff(ledx2);
+}
+
+/**
+ * Turns on three specific LEDs.
+ *
+ * @param ledx1 The index of the first LED to turn on (1-5).
+ * @param ledx2 The index of the second LED to turn on (1-5).
+ * @param ledx3 The index of the third LED to turn on (1-5).
+ */
+void turnOn(int ledx1, int ledx2, int ledx3) {
+  turnOn(ledx1);
+  turnOn(ledx2);
+  turnOn(ledx3);
+}
+
+/**
+ * Turns off three specific LEDs.
+ *
+ * @param ledx1 The index of the first LED to turn off (1-5).
+ * @param ledx2 The index of the second LED to turn off (1-5).
+ * @param ledx3 The index of the third LED to turn off (1-5).
+ */
+void turnOff(int ledx1, int ledx2, int ledx3) {
+  turnOff(ledx1);
+  turnOff(ledx2);
+  turnOff(ledx3);
+}
+
+/**
+ * Turns on four specific LEDs.
+ *
+ * @param ledx1 The index of the first LED to turn on (1-5).
+ * @param ledx2 The index of the second LED to turn on (1-5).
+ * @param ledx3 The index of the third LED to turn on (1-5).
+ * @param ledx4 The index of the fourth LED to turn on (1-5).
+ */
+void turnOn(int ledx1, int ledx2, int ledx3, int ledx4) {
+  turnOn(ledx1);
+  turnOn(ledx2);
+  turnOn(ledx3);
+  turnOn(ledx4);
+}
+
+/**
+ * Turns off four specific LEDs.
+ *
+ * @param ledx1 The index of the first LED to turn off (1-5).
+ * @param ledx2 The index of the second LED to turn off (1-5).
+ * @param ledx3 The index of the third LED to turn off (1-5).
+ * @param ledx4 The index of the fourth LED to turn off (1-5).
+ */
+void turnOff(int ledx1, int ledx2, int ledx3, int ledx4) {
+  turnOff(ledx1);
+  turnOff(ledx2);
+  turnOff(ledx3);
+  turnOff(ledx4);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,70 +1,36 @@
 #include <Arduino.h>
 
-#include "Mybase64.h"
-#include "cameraop.h"
-#include "fileop.h"
-#include "mqttop.h"
+#include "easylight.h"
 #include "wifiop.h"
 
-///// Configurations /////
-// Instantiate the netconfig.json from the template and store it in data folder.
-String config_file = "/netconfig.json";
 
-// WiFi MQTT Global Variables
-String wifi_ssid;
-String wifi_password;
-String mqtt_server;
-int mqtt_port;
-
-// Pins Declaration
-const int ledOnBoard = 33;
 
 /// @function: Entry point
 
 void setup() {
-  pinMode(ledOnBoard, OUTPUT);
-  digitalWrite(ledOnBoard, HIGH);
+  setupLights();
+
   Serial.begin(9600);
   Serial.println("Start");
+  turnOn();
+  delay(400);
+  turnOff();
+  delay(400);
+  turnOn(1);
+  delay(400);
+  turnOff(1);
+  delay(400);
+  turnOn(2, 4, 5);
+  delay(400);
+  turnOff(2, 4);
+  delay(400);
+  turnOn(1, 2, 3);
+  delay(400);
+  turnOff();
 
-  if (loadConfig(config_file, wifi_ssid, wifi_password, mqtt_server,
-                 mqtt_port)) {
-    
-    scanfWifi();
-    setupWifi(wifi_ssid.c_str(), wifi_password.c_str());
-    mqttSetup(mqtt_server.c_str(), mqtt_port);
-    setupCamera();
-    // setup done, turn on the led
-    digitalWrite(ledOnBoard, LOW);
-  } else {
-    abort();
-  }
+  scanfWifi();
 }
 
 /// @function: Main loop
 
-void loop() {
-  mqttReconnect();
-  camera_fb_t* frame_buffer = esp_camera_fb_get();
-
-  if (frame_buffer) {
-    Serial.printf("width: %d, height: %d, buf: 0x%x, len: %d\n",
-                  frame_buffer->width, frame_buffer->height, frame_buffer->buf,
-                  frame_buffer->len);
-    char* input = (char*)frame_buffer->buf;
-    char output[base64_enc_len(3)];
-    String imageFile = "data:image/jpeg;base64,";
-
-    for (int i = 0; i < frame_buffer->len; i++) {
-      base64_encode(output, (input++), 3);
-      if (i % 3 == 0) imageFile += urlencode(String(output));
-    }
-    // Serial.println(imageFile);
-    esp_camera_fb_return(frame_buffer);
-
-    mqttPublish(imageFile);
-  }
-
-  delay(20000);  // [ms]
-  Serial.println("Get it.");
-}
+void loop() { flowlights(); }
