79ca6d34466d871f5205207e023db61e598cb955
Eirin <Eirin_lin@outlook.com>
2023-11-01 12:19:50 +0800

    nothing

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 #include <Arduino.h>
-
 #include "Mybase64.h"
 #include "cameraop.h"
 #include "fileop.h"
 #include "mqttop.h"
 #include "wifiop.h"
-
+#include "HardwareSerial.h"
+#include <BLAKE2s.h>
 ///// Configurations /////
 // Instantiate the netconfig.json from the template and store it in data folder.
 String config_file = "/netconfig.json";
@@ -18,53 +18,77 @@ int mqtt_port;
 
 // Pins Declaration
 const int ledOnBoard = 33;
+HardwareSerial myserial(1); // use UART1
 
 /// @function: Entry point
 
-void setup() {
+void setup()
+{
   pinMode(ledOnBoard, OUTPUT);
   digitalWrite(ledOnBoard, HIGH);
-  Serial.begin(9600);
-  Serial.println("Start");
+  Serial.begin(115200);
+  // RX TX
+  myserial.begin(115200, SERIAL_8N1, 14, 15);
 
-  if (loadConfig(config_file, wifi_ssid, wifi_password, mqtt_server,
-                 mqtt_port)) {
-    
-    scanfWifi();
-    setupWifi(wifi_ssid.c_str(), wifi_password.c_str());
-    mqttSetup(mqtt_server.c_str(), mqtt_port);
-    setupCamera();
-    // setup done, turn on the led
-    digitalWrite(ledOnBoard, LOW);
-  } else {
-    abort();
-  }
+  setupCamera();
+  // setup done, turn on the led
+  digitalWrite(ledOnBoard, LOW);
+  delay(10000);
 }
 
 /// @function: Main loop
 
-void loop() {
-  mqttReconnect();
-  camera_fb_t* frame_buffer = esp_camera_fb_get();
+void loop()
+{
+  camera_fb_t *frame_buffer = esp_camera_fb_get();
 
-  if (frame_buffer) {
-    Serial.printf("width: %d, height: %d, buf: 0x%x, len: %d\n",
-                  frame_buffer->width, frame_buffer->height, frame_buffer->buf,
-                  frame_buffer->len);
-    char* input = (char*)frame_buffer->buf;
+  if (frame_buffer)
+  {
+    myserial.printf("width: %d, height: %d, buf: 0x%x, len: %d\r\n",
+                    frame_buffer->width, frame_buffer->height, frame_buffer->buf,
+                    frame_buffer->len);
+    char *input = (char *)frame_buffer->buf;
     char output[base64_enc_len(3)];
     String imageFile = "data:image/jpeg;base64,";
-
-    for (int i = 0; i < frame_buffer->len; i++) {
+    for (int i = 0; i < frame_buffer->len; i++)
+    {
       base64_encode(output, (input++), 3);
-      if (i % 3 == 0) imageFile += urlencode(String(output));
+      if (i % 3 == 0)
+        imageFile += urlencode(String(output));
     }
-    // Serial.println(imageFile);
     esp_camera_fb_return(frame_buffer);
+    int segmentSize = 1024;
+    int stringLength = imageFile.length();
+    for (int i = 0; i < stringLength; i += segmentSize)
+    {
+      String segment;
+      if (i + segmentSize < stringLength)
+      {
+        segment = imageFile.substring(i, i + segmentSize);
+      }
+      else
+      {
+        segment = imageFile.substring(i);
+      }
 
-    mqttPublish(imageFile);
+      // 发送串口消息说明是第几段和片段长度
+      int segmentNumber = i / segmentSize + 1;
+      int segmentLength = segment.length();
+      myserial.print("Segment ");
+      myserial.print(segmentNumber);
+      Serial.print(segmentNumber);
+      myserial.print(" (Length: ");
+      myserial.print(segmentLength);
+      Serial.print(segmentLength);
+      myserial.print("):");
+      myserial.print("\r\n");
+      Serial.print("\r\n");
+      delay(100);
+      myserial.print(segment);
+      myserial.print("\r\n");
+      delay(100);
+    }
+    //  myserial.print("\r\n");
   }
-
-  delay(20000);  // [ms]
-  Serial.println("Get it.");
+  delay(120000); // [ms]
 }
