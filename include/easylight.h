669247372d04b0afad7eeb6313c3c07de99e515c
Qianmo <92790790+Qianmoxsn@users.noreply.github.com>
2023-11-11 17:14:24 +0800

    Feat：test cam pins

--- include/easylight.h
+++ include/easylight.h
@@ -2,14 +2,30 @@
 
 #include <Arduino.h>
 
-#define LED_1_PIN 2
-#define LED_2_PIN 15
-#define LED_3_PIN 16
-#define LED_4_PIN 11
-#define LED_5_PIN 10
+#define LED_1_PIN 42
+#define LED_2_PIN 41
+#define LED_3_PIN 40
+#define LED_4_PIN 39
+#define LED_5_PIN 21
+
+#define CAM_1 4
+#define CAM_2 5
+#define CAM_3 6
+#define CAM_4 7
+#define CAM_5 15
+#define CAM_6 16
+#define CAM_7 17
+#define CAM_8 18
+#define CAM_9 8
+#define CAM_10 9
+#define CAM_11 10
+#define CAM_12 11
+#define CAM_13 12
+#define CAM_14 13
 
 // creat a list of led_pin_list
 constexpr int led_pin_list[5] = {LED_1_PIN, LED_2_PIN, LED_3_PIN, LED_4_PIN, LED_5_PIN};
+constexpr int cam_pin_list[14] = {CAM_1, CAM_2, CAM_3, CAM_4, CAM_5, CAM_6, CAM_7, CAM_8, CAM_9, CAM_10, CAM_11, CAM_12, CAM_13, CAM_14};
 
 #define LED_ON HIGH
 #define LED_OFF LOW
@@ -27,3 +43,8 @@ void turnOn(int, int, int);
 void turnOff(int, int, int);
 void turnOn(int, int, int, int);
 void turnOff(int, int, int, int);
+
+void setupcamLights();
+void flowcamlights();
+void turnoncamlights();
+void turnoffcamlights();
--- src/main.cpp
+++ src/main.cpp
@@ -12,25 +12,30 @@ void setup() {
 
   Serial.begin(9600);
   Serial.println("Start");
+  turnoncamlights();
+
   turnOn();
   delay(400);
   turnOff();
-  delay(400);
-  turnOn(1);
-  delay(400);
-  turnOff(1);
-  delay(400);
-  turnOn(2, 4, 5);
-  delay(400);
-  turnOff(2, 4);
-  delay(400);
-  turnOn(1, 2, 3);
+  turnOn(1, 3, 5);
   delay(400);
   turnOff();
+  delay(400);
+  turnOn(2, 4);
+  delay(400);
+  turnOff();
+
+
+  turnoffcamlights();
 
   scanfWifi();
 }
 
 /// @function: Main loop
 
-void loop() { flowlights(); }
+void loop() {
+  flowlights();
+  delay(500);
+  flowcamlights();
+  delay(500);
+}
